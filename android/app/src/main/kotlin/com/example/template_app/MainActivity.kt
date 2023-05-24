38e2b5014f858a1ef21151a93415e31882a225de
huuln.tgg <huuln.tgg@vnpt.vn>
2023-05-24 16:11:16 +0700

    revert

--- android/app/src/main/kotlin/com/example/template_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.template_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/controller/counter_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class CounterController extends GetxController {
-  RxInt count = 0.obs;
-
-  void increment() {
-    count.value++;
-  }
-}
